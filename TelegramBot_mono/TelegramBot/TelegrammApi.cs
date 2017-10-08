f5c8d25099f1c57daf4559996dd1f36f09a7ea7e
Piter <pitr21@mail.ru>
2017-10-08 15:40:15 +0300

    1

---
+++ TelegramBot_mono/TelegramBot/TelegrammApi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+
+namespace TelegramBot
+{
+	public class TelegrammApi
+	{
+		public TelegrammApi ()
+		{
+		}
+
+
+		public static string Token { set; get; } = @"452723579:AAFVaFR6RidJKHAy8j7xn1jAVJilGgY8PNU";
+		public static int LastUpdateID { set; get; } = 0;
+
+		static void SendMessage(string message, int chatid)
+		{
+			using (var webClient = new WebClient())
+			{
+				var pars = new NameValueCollection();
+
+				pars.Add("text", message);
+				pars.Add("chat_id", chatid.ToString());
+
+
+				webClient.UploadValues("https://api.telegram.org/bot" + Token + "/sendMessage", pars);
+
+			}
+		}
+
+		public async static Task SendPhoto(string chatId, string filePath)
+		{
+			var url = string.Format("https://api.telegram.org/bot{0}/sendPhoto", Token);
+			var fileName = filePath.Split('\\').Last();
+
+			using (var form = new MultipartFormDataContent())
+			{
+				form.Add(new StringContent(chatId.ToString(), Encoding.UTF8), "chat_id");
+
+				using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+				{
+					form.Add(new StreamContent(fileStream), "photo", fileName);
+
+					using (var client = new HttpClient())
+					{
+						await client.PostAsync(url, form);
+					}
+				}
+			}
+		}
+
+	}
+}
+
